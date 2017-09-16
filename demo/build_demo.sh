6ddcb8b3aeb4c9ebbc9bcf5f8d8e8b70a618bfba
Bryan Christ <bryan.christ@gmail.com>
2017-09-15 20:57:27 -0500

    build with CMake

--- demo/build_demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PKG_CFG_INCL=`pkg-config --cflags-only-I ncurses`
-PKG_CFG_LIBS=`pkg-config --libs-only-l ncurses`
-
-rm -rf vshell
-gcc -o vshell $PKG_CFG_INCL -ggdb3 *.c $PKG_CFG_LIBS -lvterm -lutil
-
-
