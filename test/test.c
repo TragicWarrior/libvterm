8ea7edc5d4d3eb14074dc357c6e36b920241f764
Idan Shoham <idan@hitachi-id.com>
2017-12-29 13:44:35 -0700

    Working on terminal recording bit.

--- test/test.c
+++ test/test.c
@@ -32,6 +32,7 @@ dimensions are the same as the dimensions in the capture file.
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <ctype.h>
 
 #define BUFLEN 500
 
@@ -195,6 +196,180 @@ void CursorTopLeft()
   fputs( "1;1H", stdout );
   }
 
+int lastW = -1;
+int lastH = -1;
+vterm_cell_t** lastBuffer = NULL;
+FILE* changeLog = NULL;
+time_t tStart = 0;
+
+void FatalError( char* msg )
+  {
+  fputs( "FATAL ERROR: ", stderr );
+  fputs( msg, stderr );
+  fputs( "\n", stderr );
+  exit(-99);
+  }
+
+void InitPrivateBuffer( int w, int h )
+  {
+  int row, col;
+  lastBuffer = (vterm_cell_t**)calloc(h, sizeof(vterm_cell_t*));
+  if( lastBuffer==NULL )
+    {
+    FatalError("Allocating private buffer");
+    }
+  for( row=0; row<h; row++ )
+    {
+    vterm_cell_t *ptr
+      = lastBuffer[row]
+      = (vterm_cell_t*)calloc(w, sizeof(vterm_cell_t));
+    if( lastBuffer[row]==NULL )
+      {
+      FatalError("Allocating private buffer row");
+      }
+    for( col=0; col<w; col++ )
+      {
+      ptr->ch = ' ';
+      ++ptr;
+      }
+    }
+  lastW = w;
+  lastH = h;
+
+  if( changeLog==NULL )
+    {
+    changeLog = fopen("/tmp/session-capture.log","w");
+    if( changeLog==NULL )
+      {
+      FatalError("Opening change log file");
+      }
+    }
+
+  tStart = time(NULL);
+  }
+
+void ResizePrivateBuffer( int w, int h )
+  {
+  int row;
+  if( lastBuffer != NULL )
+    {
+    for( row=0; row<lastH; row++ )
+      {
+      if( lastBuffer[row]!=NULL )
+        {
+        free( lastBuffer[row] );
+        lastBuffer[row] = NULL;
+        }
+      }
+    free( lastBuffer );
+    lastBuffer = NULL;
+    lastW = -1;
+    lastH = -1;
+    }
+  InitPrivateBuffer( w, h );
+  }
+
+void StartDiff( int row, int col )
+  {
+  fprintf( changeLog, "@%03d,%03d\n", row, col );
+  }
+
+void PrintChar( int c )
+  {
+  if( isalnum( c ) || c==' ' )
+    {
+    fputc( c, changeLog );
+    }
+  else
+    {
+    fprintf( changeLog, "%%%02x", c );
+    }
+  }
+
+void PrintAttr( int attr )
+  {
+  fprintf( changeLog, "\\%04x", attr );
+  }
+
+void PrintBufferDiffs( int w, int h, vterm_cell_t** cells )
+  {
+  int row=0, col=0, inDiff=0, diffC=0, diffA=0, diff=0;
+  vterm_cell_t *lastRow=NULL, *currentRow=NULL;
+  if( cells==NULL || lastBuffer == NULL || w!=lastW || h!=lastH || w<0 || h<0 )
+    {
+    FatalError("Cannot process diffs across mismatched buffers");
+    }
+
+  // print a heartbeat
+  time_t tNow = time(NULL);
+  fprintf( changeLog, ":%ld\n", (unsigned long)(tNow - tStart));
+
+  for( row=0; row<h; ++row )
+    {
+    lastRow = lastBuffer[row];
+    currentRow = cells[row];
+    inDiff = 0;
+    for( col=0; col<w; ++col )
+      {
+      diffC = (lastRow->ch == currentRow->ch) ? 0 : 1;
+      diffA = (lastRow->attr == currentRow->attr) ? 0 : 1;
+      diff = diffC || diffA;
+
+      if( inDiff )
+        { // in the middle of printing a diff
+        if( diff )
+          {
+          if( diffC )
+            {
+            PrintChar( currentRow->ch );
+            lastRow->ch = currentRow->ch;
+            }
+          if( diffA )
+            {
+            PrintAttr( currentRow->attr );
+            lastRow->attr = currentRow->attr;
+            }
+          }
+        else
+          {
+          fputc( '\n', changeLog );
+          inDiff = 0;
+          }
+        }
+      else
+        { // not currently printing a diff
+        if( diff )
+          {
+          inDiff = 1;
+          StartDiff( row, col );
+          if( diffC )
+            {
+            PrintChar( currentRow->ch );
+            lastRow->ch = currentRow->ch;
+            }
+          if( diffA )
+            {
+            PrintAttr( currentRow->attr );
+            lastRow->attr = currentRow->attr;
+            }
+          }
+        else
+          {
+          // do nothing.
+          }
+        }
+      ++lastRow;
+      ++currentRow;
+      } // end of row
+
+    if( inDiff )
+      {
+      fputc( '\n', changeLog );
+      inDiff = 0;
+      }
+    } // rows
+  }
+
 void PrintTermBuffer( vterm_t* vt )
   {
   int h = -1;
@@ -215,6 +390,18 @@ void PrintTermBuffer( vterm_t* vt )
   vterm_cell_t** cells = vterm_get_buffer( vt );
   if( cells!=NULL )
     {
+
+    if( lastW==-1 )
+      {
+      InitPrivateBuffer( w, h );
+      }
+    else if( lastW!=w || lastH!=h )
+      {
+      ResizePrivateBuffer( w, h );
+      }
+
+    PrintBufferDiffs( w, h, cells );
+
     int i=0, j=0, c=-1, a=-1;
     ResetAttribute();
     for( i=0; i<h; ++i )
