e9b5e0866b870a4189813dcb77c63771343fb52b
Bryan Christ <bryan.christ@gmail.com>
2018-12-24 12:20:12 -0600

    handle CSI for cursor absolute positioning

--- vterm_csi_CUx.c
+++ vterm_csi_CUx.c
@@ -28,7 +28,8 @@ This library is based on ROTE written by Bruno Takahashi C. de Oliveira
 
 /* Interpret the 'relative mode' sequences: CUU, CUD, CUF, CUB, CNL,
  * CPL, CHA, HPR, VPA, VPR, HPA */
-void interpret_csi_CUx(vterm_t *vterm,char verb,int param[],int pcount)
+void
+interpret_csi_CUx(vterm_t *vterm, char verb, int param[], int pcount)
 {
     vterm_desc_t    *v_desc = NULL;
     int             idx;
@@ -63,6 +64,30 @@ void interpret_csi_CUx(vterm_t *vterm,char verb,int param[],int pcount)
         case 'G':
         case '`':   v_desc->ccol = param[0] - 1;   break;
         case 'd':   v_desc->crow = param[0] - 1;   break;
+
+        /*
+            ESC [ r ; c H
+            where 'r' is row number and 'c' is column number
+            - or -
+            ESC [ H
+            which means 0, 0 implied
+        */
+        case 'H':
+        {
+            if(pcount == 0)
+            {
+                v_desc->crow = 0;
+                v_desc->ccol = 0;
+                break;
+            }
+
+            v_desc->crow = param[0] - 1;
+            v_desc->ccol = param[1] - 1;
+            // endwin();
+            // printf("r:%d, c:%d\n\r", param[0] - 1, param[1] - 1);
+            // exit(0);
+            break;
+        }
     }
 
     clamp_cursor_to_bounds(vterm);
