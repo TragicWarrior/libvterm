c5bbe126f271920d132f9d5b8ba1863db4f3c940
Bryan Christ <bryan.christ@gmail.com>
2019-03-19 12:22:59 -0500

    bump to version 4.44 and update changelog

--- vterm.h
+++ vterm.h
@@ -60,7 +60,7 @@ typedef chtype          attr_t;
 #include <ncursesw/curses.h>
 #endif
 
-#define LIBVTERM_VERSION        "4.43"
+#define LIBVTERM_VERSION        "4.44"
 
 #define VTERM_FLAG_RXVT         (1 << 0)    // masquerade as rxvt (default)
 #define VTERM_FLAG_VT100        (1 << 1)    // masquerade as vt100
