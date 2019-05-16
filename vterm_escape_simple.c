cd719026019d796f90bedef011708ed2545b1628
Bryan Christ <bryan.christ@gmail.com>
2019-05-15 21:33:26 -0500

    add file new file for prev commits

---
+++ vterm_escape_simple.c
@@ -0,0 +1,89 @@
+#include "macros.h"
+#include "vterm_private.h"
+#include "vterm_escape.h"
+#include "vterm_csi.h"
+
+// returns number of bytes processed (should always be 0 or 1)
+
+int
+vterm_interpret_escapes_simple(vterm_t *vterm)
+{
+    char            firstchar;
+
+    static void     *simple_table[128] =
+                        {
+                            [0] = &&simple_char_default,
+                            ['E'] = &&simple_char_E,
+                            ['M'] = &&simple_char_M,
+                            ['A'] = &&simple_char_A,
+                            ['B'] = &&simple_char_B,
+                            ['C'] = &&simple_char_C,
+                            ['D'] = &&simple_char_D,
+                            ['7'] = &&simple_char_vii,
+                            ['8'] = &&simple_char_viii,
+                            ['c'] = &&simple_char_c,
+                        };
+
+    firstchar = vterm->esbuf[0];
+
+    SWITCH(simple_table, (unsigned int)firstchar, 0);
+
+    // interpert ESC-M a line-feed (NEL)
+    simple_char_E:
+        interpret_esc_NEL(vterm);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // interpret ESC-M as reverse line-feed (RI)
+    simple_char_M:
+        interpret_esc_RI(vterm);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // VT52, move cursor up.  same as CUU which is ESC [ A
+    simple_char_A:
+        interpret_csi_CUx(vterm, 'A', (int *)NULL, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // VT52, move cursor down.  same as CUD which is ESC [ B
+    simple_char_B:
+        interpret_csi_CUx(vterm, 'B', (int *)NULL, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // VT52, move cursor right.  same as CUF which is ESC [ C
+    simple_char_C:
+        interpret_csi_CUx(vterm, 'C', (int *)NULL, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // VT52, move cursor left.  same as CUB which is ESC [ D
+    simple_char_D:
+        interpret_csi_CUx(vterm, 'D', (int *)NULL, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    simple_char_vii:
+        interpret_csi_SAVECUR(vterm, 0, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    simple_char_viii:
+        interpret_csi_RESTORECUR(vterm, 0, 0);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // The ESC c sequence is RS1 reset for most
+    simple_char_c:
+        // push in "\ec" as a safety check
+        interpret_csi_RS1_xterm(vterm, XTERM_RS1);
+        vterm_escape_cancel(vterm);
+        return 1;
+
+    // unhandled sequence
+    simple_char_default:
+
+    // zero means no sequences processed
+    return 0;
+}
