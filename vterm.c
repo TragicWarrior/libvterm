5826ab8ba87f9a95b7c0b8c0c32c14f8bc862d1b
Bryan Christ <bryanc@Bryans-Mac-mini.local>
2019-06-04 09:33:56 -0500

    fixed include path error

--- vterm.c
+++ vterm.c
@@ -21,7 +21,7 @@
 #if defined(__APPLE__) && defined(__MACH__)     // forkpty() in osx
 #include <util.h>
 #else
-#include <ncurses/include/term.h>
+#include <term.h>
 #endif
 
 #include <sys/types.h>
