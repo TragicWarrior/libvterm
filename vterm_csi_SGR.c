1d209bb56a474b1667e082386207a31bf8fbcd96
Bryan Christ <bryan.christ@gmail.com>
2019-06-21 16:37:05 -0500

    remove code added for debugging of ACS

--- vterm_csi_SGR.c
+++ vterm_csi_SGR.c
@@ -61,11 +61,6 @@ interpret_csi_SGR(vterm_t *vterm, int param[], int pcount)
     {
         // reset attributes
         v_desc->curattr = A_NORMAL;
-        vterm->internal_state &= ~(STATE_ALT_CHARSET);
-
-        // v_desc->default_colors = COLOR_PAIR(0);
-        // v_desc->fg = COLOR_WHITE;
-        // v_desc->bg = COLOR_BLACK;
 
         _vterm_set_color_pair_safe(vterm, v_desc->default_colors,
             v_desc->fg, v_desc->bg);
@@ -82,7 +77,6 @@ interpret_csi_SGR(vterm_t *vterm, int param[], int pcount)
             {
                 // reset attributes
                 v_desc->curattr = A_NORMAL;
-                vterm->internal_state &= ~(STATE_ALT_CHARSET);
 
                 _vterm_set_color_pair_safe(vterm, v_desc->default_colors,
                     v_desc->fg, v_desc->bg);
