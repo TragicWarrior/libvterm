a1647de31723f4353b0870d0a8e40a08b416518b
Bryan Christ <bryan.christ@gmail.com>
2019-07-04 15:36:52 -0500

    bump to version 5.15 and update changelog

--- vterm.h
+++ vterm.h
@@ -72,7 +72,7 @@ typedef chtype          attr_t;
 
 #endif
 
-#define LIBVTERM_VERSION        "5.14"
+#define LIBVTERM_VERSION        "5.15"
 
 #define VTERM_FLAG_RXVT         (1UL << 0)      //  emulate rxvt
 #define VTERM_FLAG_VT100        (1UL << 1)      //  emulate vt100
