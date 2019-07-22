002f2ddbbfc5903758aee79ee421560e3f3a4528
Bryan Christ <bryan.christ@gmail.com>
2019-07-22 18:29:15 -0500

    fix DCH

--- vterm_csi_DCH.c
+++ vterm_csi_DCH.c
@@ -14,6 +14,7 @@ interpret_csi_DCH(vterm_t *vterm, int param[], int pcount)
     int             c;
     int             n = 1;
     int             idx;
+    int             max_col;
 
     if(pcount && param[0] > 0) n = param[0];
 
@@ -23,25 +24,29 @@ interpret_csi_DCH(vterm_t *vterm, int param[], int pcount)
 
     vcell_new = &v_desc->cells[v_desc->crow][v_desc->ccol];
 
-    for(c = v_desc->ccol; c < v_desc->cols; c++)
+    max_col = v_desc->ccol + n;
+
+    // for(c = v_desc->ccol; c < v_desc->cols; c++)
+    for(c = v_desc->ccol; c < max_col; c++)
     {
-        if(c + n < v_desc->cols)
-        {
-            vcell_old = vcell_new + n;
+        // if(c + n < v_desc->cols)
+        // {
+            // vcell_old = vcell_new + n;
 
             /*
                 this is a shallow struct copy.  if a vterm_cell_t ever becomes
                 packed with heap data referenced by pointer, it could
                 be problematic.
             */
-            *vcell_new = *vcell_old;
-        }
-        else
-        {
+            // *vcell_new = *vcell_old;
+        // }
+        // else
+        // {
             VCELL_SET_CHAR((*vcell_new), ' ');
-            VCELL_SET_ATTR((*vcell_new), v_desc->curattr);
-            VCELL_SET_COLORS((*vcell_new), v_desc);
-        }
+            // VCELL_SET_ATTR((*vcell_new), v_desc->curattr);
+            VCELL_SET_ATTR((*vcell_new), A_NORMAL);
+            // VCELL_SET_COLORS((*vcell_new), v_desc);
+        // }
 
         vcell_new++;
     }
