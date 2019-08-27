caf2e7f379489739becce6ed508003114038eaf6
Bryan Christ <bryan.christ@gmail.com>
2019-08-27 11:07:05 -0500

    revert mouse changes.  unexpected fallout

--- mouse_driver.c
+++ mouse_driver.c
@@ -8,18 +8,12 @@
 
 #define EVERY_MOUSE_EVENT   (ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION)
 
-/*
-    this var has to exist in the global namespace so we'll tack on the
-    date to miminize risk of collission.
-*/
-#define MSTATE_TRACKER  mouse_state_##__DATE__
-
-mouse_state_t   *MSTATE_TRACKER  = NULL;
+mouse_state_t   *mouse_state = NULL;
 
 void
 mouse_driver_init(vterm_t *vterm)
 {
-    extern mouse_state_t    *MSTATE_TRACKER;
+    extern mouse_state_t    *mouse_state;
 
     mmask_t                 mouse_mask = EVERY_MOUSE_EVENT;
 
@@ -32,15 +26,15 @@ mouse_driver_init(vterm_t *vterm)
         instance.  when it's not null it almost certainly means we're
         in a shared environment (like the VWM window manager).
     */
-    if(MSTATE_TRACKER == NULL)
+    if(mouse_state == NULL)
     {
-        MSTATE_TRACKER = calloc(1, sizeof(mouse_state_t));
+        mouse_state = calloc(1, sizeof(mouse_state_t));
 
         if(has_mouse() == TRUE)
-            MSTATE_TRACKER->origin = NULL;
+            mouse_state->origin = NULL;
         else
         {
-            MSTATE_TRACKER->origin = vterm;
+            mouse_state->origin = vterm;
 
             /*
                 as the instantiator, we can set this up however we want
@@ -51,12 +45,10 @@ mouse_driver_init(vterm_t *vterm)
         }
     }
 
-    MSTATE_TRACKER->ref_count++;
+    mouse_state->ref_count++;
 
     if(vterm->mouse_config == NULL)
-    {
-        vterm->mouse_config = calloc(1, sizeof(struct _mouse_config_s));
-    }
+        vterm->mouse_config = calloc(1, sizeof(mouse_config_t));
 
     return;
 }
@@ -125,7 +117,7 @@ mouse_driver_default(vterm_t *vterm, unsigned char *buf)
 void
 mouse_driver_save_state(vterm_t *vterm)
 {
-    extern mouse_state_t    *MSTATE_TRACKER;
+    extern mouse_state_t    *mouse_state;
 
     mmask_t     mouse_mask = EVERY_MOUSE_EVENT;
 
@@ -133,7 +125,7 @@ mouse_driver_save_state(vterm_t *vterm)
         if the mouse origin is anyone but this instance, then we need to
         save the previous mask and interval so we can restore it later.
     */
-    if(MSTATE_TRACKER->origin != vterm)
+    if(mouse_state->origin != vterm)
     {
         mousemask(mouse_mask, &vterm->mouse_config->mouse_mask);
         vterm->mouse_config->mouse_interval = mouseinterval(0);
@@ -145,13 +137,13 @@ mouse_driver_save_state(vterm_t *vterm)
 void
 mouse_driver_restore_state(vterm_t *vterm)
 {
-    extern mouse_state_t    *MSTATE_TRACKER;
+    extern mouse_state_t    *mouse_state;
 
     /*
         if the mouse origin is anyone but this instance, then we need to
         restore the saved mask and interval.
     */
-    if(MSTATE_TRACKER->origin != vterm)
+    if(mouse_state->origin != vterm)
     {
         mousemask(vterm->mouse_config->mouse_mask, NULL);
         mouseinterval(vterm->mouse_config->mouse_interval);
@@ -163,9 +155,7 @@ mouse_driver_restore_state(vterm_t *vterm)
 void
 mouse_driver_free(vterm_t *vterm)
 {
-    extern mouse_state_t    *MSTATE_TRACKER;
-
-    if(vterm == NULL) return;
+    extern mouse_state_t    *mouse_state;
 
     // release the mouse config
     if(vterm->mouse_config != NULL)
@@ -174,14 +164,14 @@ mouse_driver_free(vterm_t *vterm)
         vterm->mouse_config = NULL;
     }
 
-    if(MSTATE_TRACKER != NULL)
+    if(mouse_state != NULL)
     {
         // tear down the reference count
-        MSTATE_TRACKER->ref_count--;
+        mouse_state->ref_count--;
 
-        if(MSTATE_TRACKER->ref_count == 0)
+        if(mouse_state->ref_count == 0)
         {
-            if(MSTATE_TRACKER != NULL)
+            if(mouse_state->origin != NULL)
             {
                 mousemask(0, NULL);
             }
