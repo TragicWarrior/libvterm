cc0fa1b99f1b9d765d678ec8180c331d52be48c4
Bryan Christ <bryan.christ@gmail.com>
2019-09-02 12:59:29 -0500

    fix compiler warning and comment out some code not needed

--- vterm_esc_IND.c
+++ vterm_esc_IND.c
@@ -11,13 +11,6 @@
 inline void
 interpret_esc_IND(vterm_t *vterm)
 {
-    vterm_desc_t    *v_desc = NULL;
-    int             idx;
-
-    // set active vterm description selector
-    idx = vterm_buffer_get_active(vterm);
-    v_desc = &vterm->vterm_desc[idx];
-
     vterm_scroll_up(vterm, FALSE);
 
     return;
