688b5c5ee55427aa251413dff9828902d3610be2
Leon Winter <lwi@ring0.de>
2019-12-10 17:51:45 +0100

    detect C1 8-bit control characters

--- vterm_ctrl_char.h
+++ vterm_ctrl_char.h
@@ -1,8 +1,12 @@
 #ifndef _VTERM_CTRL_CHAR_H_
 #define _VTERM_CTRL_CHAR_H_
 
-#define IS_CTRL_CHAR(x) \
+#define IS_C0(x) \
             ((unsigned int)x >= 1 && (unsigned int)x <= 31)
+#define IS_C1(x) \
+            ((unsigned int)x >= 128 && (unsigned int)x <= 159)
+#define IS_CTRL_CHAR(x) \
+            (IS_C0(x) || IS_C1(x))
 
 void    vterm_interpret_ctrl_char(vterm_t *vterm, char *data);
 
