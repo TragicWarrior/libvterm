9dbf7046f29b73f285daabcae5cd5ca6822d2315
Bryan Christ <bryan.christ@gmail.com>
2019-12-26 15:37:55 -0600

    bump to version 8.10 and update changelog

--- vterm.h
+++ vterm.h
@@ -28,7 +28,7 @@
 #undef FALSE
 #define FALSE           0
 
-#define LIBVTERM_VERSION        "8.9"
+#define LIBVTERM_VERSION        "8.10"
 
 #define VTERM_FLAG_RXVT         (1UL << 0)      //  emulate rxvt
 #define VTERM_FLAG_VT100        (1UL << 1)      //  emulate vt100
